97dcd10ce9501678d8ae832ff871d34c44c048e3
Dave Lambley <dave@lambley.me.uk>
2025-02-14 17:47:43 +0000

    Add manifest and others

--- wadweek-4-bus-times/README.md
+++ wadweek-4-bus-times/README.md
@@ -1,6 +1,6 @@
-# WAD - week 4 bus times
+# WAD - week 4 bus times - manifested
 
 A Pen created on CodePen.
 
-Original URL: [https://codepen.io/davel-the-encoder/pen/pvzXyPo](https://codepen.io/davel-the-encoder/pen/pvzXyPo).
+Original URL: [https://codepen.io/davel-the-encoder/pen/NPWqbyg](https://codepen.io/davel-the-encoder/pen/NPWqbyg).
 
--- wadweek-4-bus-times/dist/script.js
+++ wadweek-4-bus-times/dist/script.js
@@ -1,6 +1,7 @@
 import React from "https://esm.sh/react@19?development";
 import ReactDOMClient from "https://esm.sh/react-dom@19/client/?development";
 
+// https://v5.mui.com/material-ui/all-components/
 import {
 Button,
 FormControl,
@@ -26,66 +27,53 @@ Container,
 CssBaseline } from
 "https://esm.sh/@mui/material@5";
 
+// https://v5.mui.com/material-ui/material-icons/
 import MenuIcon from "https://esm.sh/@mui/icons-material/Menu";
 
-// https://content.tfl.gov.uk/tfl-live-bus-river-bus-arrivals-api-documentation.pdf
-
-const BusTime = props => {
-  let wait = "Due";
-  if (props.waitingTime >= 1) {
-    const minutes = Math.floor(props.waitingTime / 60000);
-    const seconds = Math.floor((props.waitingTime - minutes * 60000) / 1000);
+// This function takes a the estimated time of a bus's arrival and the current
+// time and returns a string with the time as text formatted in minutes and seconds.
+function formatBusTime(estimatedEta, epochTime) {
+  const waitingTime = estimatedEta - epochTime;
+  if (waitingTime >= 1) {
+    const minutes = Math.floor(waitingTime / 60000);
+    const seconds = Math.floor((waitingTime - minutes * 60000) / 1000);
 
     // Messy, but seconds is always positive so it works.
     if (seconds < 10) {
-      wait = minutes + ":0" + seconds;
-    } else {
-      wait = minutes + ":" + seconds;
+      return minutes + ":0" + seconds;
     }
+    return minutes + ":" + seconds;
   }
-  return /*#__PURE__*/(
-    React.createElement(Card, null, /*#__PURE__*/
-    React.createElement(CardContent, null, /*#__PURE__*/
-    React.createElement(Grid, { container: true, spacing: 2 }, /*#__PURE__*/
-    React.createElement(Grid, { xs: 8 }, /*#__PURE__*/
-    React.createElement(Typography, { variant: "body1" }, /*#__PURE__*/
-    React.createElement("span", { class: "busNumber" }, props.bus), " to ", props.destination)), /*#__PURE__*/
+  return "Due";
+}
 
-
-    React.createElement(Grid, { xs: 4 }, /*#__PURE__*/
-    React.createElement(Typography, { variant: "body1", align: "right" },
-    wait))))));
-
-
-
-
-
-
-};
-
+// Function to download the bus times from TFL's service and store
+// it in a React state variable.
+// https://content.tfl.gov.uk/tfl-live-bus-river-bus-arrivals-api-documentation.pdf
 function readFromTFL(stopCode, setBusTimes) {
   fetch(
   "https://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?StopCode1=" +
   stopCode +
-  "&ReturnList=DestinationName,LineName,EstimatedTime",
+  "&ReturnList=StopPointIndicator,DestinationName,LineName,EstimatedTime",
   { mode: "cors" }).
   then(response => {
     response.text().then(t => {
       const lines = t.split("\n");
-      const b = [];
+      const busList = [];
       for (const line of lines) {
         const data = JSON.parse(line);
         if (data[0] === 1) {
-          b.push({
-            number: data[1],
-            destination: data[2],
-            eta: data[3] });
+          busList.push({
+            stopPoint: data[1],
+            number: data[2],
+            destination: data[3],
+            eta: data[4] });
 
         }
       }
 
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toSorted
-      setBusTimes(b.toSorted((a, b) => a.eta - b.eta));
+      setBusTimes(busList.toSorted((a, b) => a.eta - b.eta));
     });
   });
 }
@@ -98,7 +86,6 @@ function BusTimesForStop(props) {
   React.useEffect(() => {
     readFromTFL(props.stopCode, setBusTimes);
     const reloadData = setInterval(() => {
-      console.log("reloading!");
       readFromTFL(props.stopCode, setBusTimes);
     }, 60000);
 
@@ -114,14 +101,17 @@ function BusTimesForStop(props) {
     // useEffects wants us to tell it what data we used.
   }, [props.stopCode]);
 
+  // Transform an array of JavaScript objects in to an array of React elements.
   const items = busTimes.map((bus) => /*#__PURE__*/
   React.createElement(BusTime, {
     bus: bus.number,
     destination: bus.destination,
-    waitingTime: bus.eta - epochTime }));
+    wait: formatBusTime(bus.eta, epochTime),
+    stopPoint: bus.stopPoint }));
 
 
 
+  // https://v5.mui.com/material-ui/react-stack/
   return /*#__PURE__*/(
     React.createElement(React.Fragment, null, /*#__PURE__*/
     React.createElement(Stack, { sx: { my: 2 } }, items)));
@@ -129,9 +119,36 @@ function BusTimesForStop(props) {
 
 }
 
-const App = () => {
+// https://v5.mui.com/material-ui/react-card/
+// https://v5.mui.com/material-ui/react-grid/
+// https://v5.mui.com/material-ui/react-typography/
+function BusTime(props) {
+  return /*#__PURE__*/(
+    React.createElement(Card, null, /*#__PURE__*/
+    React.createElement(CardContent, null, /*#__PURE__*/
+    React.createElement(Grid, { container: true, spacing: 2 }, /*#__PURE__*/
+    React.createElement(Grid, { xs: 8 }, /*#__PURE__*/
+    React.createElement(Typography, { variant: "body1" }, /*#__PURE__*/
+    React.createElement("span", { class: "stopPoint" }, props.stopPoint), /*#__PURE__*/
+    React.createElement("span", { class: "busNumber" }, props.bus), " to ", props.destination)), /*#__PURE__*/
+
+
+    React.createElement(Grid, { xs: 4 }, /*#__PURE__*/
+    React.createElement(Typography, { variant: "body1", align: "right" },
+    props.wait))))));
+
+
+
+
+
+
+}
+
+function MainScreen(props) {
+  // State variables and functions to handle hamburger menu.
+
+  // "Anchor element", the element which the pop up menu is linked to.
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
@@ -141,18 +158,15 @@ const App = () => {
 
   return /*#__PURE__*/(
     React.createElement(React.Fragment, null, /*#__PURE__*/
-    React.createElement(CssBaseline, null), /*#__PURE__*/
     React.createElement(AppBar, { position: "static" }, /*#__PURE__*/
     React.createElement(Toolbar, null, /*#__PURE__*/
     React.createElement(IconButton, {
       size: "large",
       edge: "start",
       color: "inherit",
-      "aria-label": "menu",
       sx: { mr: 2 },
-      "aria-controls": open ? "basic-menu" : undefined,
+      "aria-label": "menu",
       "aria-haspopup": "true",
-      "aria-expanded": open ? "true" : undefined,
       onClick: handleClick }, /*#__PURE__*/
 
     React.createElement(MenuIcon, null)), /*#__PURE__*/
@@ -161,15 +175,15 @@ const App = () => {
     React.createElement(Menu, {
       id: "basic-menu",
       anchorEl: anchorEl,
-      open: open,
+      open: Boolean(anchorEl),
       onClose: handleClose,
       MenuListProps: {
         "aria-labelledby": "basic-button" } }, /*#__PURE__*/
 
 
-    React.createElement(MenuItem, { onClick: handleClose }, "Profile"), /*#__PURE__*/
-    React.createElement(MenuItem, { onClick: handleClose }, "My account"), /*#__PURE__*/
-    React.createElement(MenuItem, { onClick: handleClose }, "Logout")), /*#__PURE__*/
+    React.createElement(MenuItem, { onClick: handleClose }, "Example"), /*#__PURE__*/
+    React.createElement(MenuItem, { onClick: handleClose }, "menu"), /*#__PURE__*/
+    React.createElement(MenuItem, { onClick: handleClose }, "items")), /*#__PURE__*/
 
 
     React.createElement(Typography, { variant: "h6", component: "div", sx: { flexGrow: 1 } }, "Barnes station bus times"))), /*#__PURE__*/
@@ -182,6 +196,15 @@ const App = () => {
 
 
 
-};
+}
+
+function App(props) {
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement(CssBaseline, null), /*#__PURE__*/
+    React.createElement(MainScreen, null)));
+
+
+}
 
 ReactDOMClient.createRoot(document.getElementById("app")).render( /*#__PURE__*/React.createElement(App, null));
